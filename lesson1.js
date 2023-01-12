3be1d0200d94f5d91c998f99cb3c6042cea0eced
DmytroSytor <dsior19930404@gmail.com>
2023-01-12 21:17:04 +0200

    Variables and data types

---
+++ lesson1.js
@@ -0,0 +1,26 @@
+// 1 завдання
+let city = "Київ";      
+city = "Львів";
+let address = city;
+alert (address);
+
+
+
+// 2 завдання
+let sum = 12 ** 3;
+console.log(sum);
+
+
+
+
+// 3 завдання
+const pen_cost = 5.25;
+const pencil_cost = 3.50;
+let pen_buy = 4;
+let pencil_buy = 6;
+all_sum_pen = pen_cost*pen_buy;
+all_sum_pencil = pencil_cost*pencil_buy;
+together = all_sum_pen + all_sum_pencil;
+document.write(together);
+
+
